ccc8cfa8d31d1d9931a74f607703a9d89da623a9
Alex Stout <astout@silvervue.com>
2016-10-06 13:26:19 -0600

    another android serial attempt

--- src/android/Device.java
+++ src/android/Device.java
@@ -20,7 +20,7 @@ package org.apache.cordova.device;
 
 import java.util.TimeZone;
 import java.lang.reflect.Method;
-import java.lang.Class<T>;
+import java.lang.Class;
 
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
